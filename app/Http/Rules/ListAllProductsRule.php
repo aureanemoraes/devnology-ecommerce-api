f725186f89a29fdbc35c31306a27aac0323f0f22
Aureane Moraes <aureane.moraes@outlook.com>
2023-04-13 14:38:26 -0300

    feat: adicionado tratamento dos filtros e do formatdo dos dados

--- app/Http/Rules/ListAllProductsRule.php
+++ app/Http/Rules/ListAllProductsRule.php
@@ -6,31 +6,33 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Arr;
 use App\Models\Supplier;
+use App\Http\Services\ProductFilterService;
+use App\Utils\FormatDataUtil;
 
 class ListAllProductsRule {
     // list all products of both suppliers
-    public function listAllProducts()
+    public function listAllProducts($filters)
     {
-
         $responses = Http::pool(function (Pool $pool) {
             Supplier::get()->each(function (Supplier $supplier) use ($pool) {
                 return $pool->as($supplier->name)->get($supplier->api_base_url);
             });
         });
 
-        if ($this->checkingIfAtLeastOneSupplierIsUp($responses)) {
-            $products = $this->getProductsFromResponses($responses);
+        if (!$this->checkingIfAtLeastOneSupplierIsUp($responses))
+            return response()->error('Suppliers API are current unavailable.', 502);
 
-            return response()->success($products);
-        }
+        $products = $this->getProductsFromResponses($responses);
 
-        return response()->error('Suppliers API are current unavailable.', 502);
+        if (count($filters) > 0) $products = (new ProductFilterService())->filter($filters, $products);
+
+        return response()->success($products);
     }
 
     private function checkingIfAtLeastOneSupplierIsUp($responses)
     {
         foreach ($responses as $response)
-            if ($response->ok()) return true;
+            if ($response->successful()) return true;
 
         return false;
     }
@@ -38,18 +40,18 @@ class ListAllProductsRule {
     // treating each product of response to grant the consistency of the data
     private function getProductsFromResponses($responses)
     {
+        $formatDataUtil = new FormatDataUtil();
         $validProducts = [];
 
         foreach($responses as $supplier => $response)
-            foreach ($response->json() as $item) $validProducts[] = $this->preventFromInvalidData($item, $supplier);
+            foreach ($response->json() as $item) $validProducts[] = $formatDataUtil->format($this->preventFromInvalidData($item, $supplier));
 
         // removing null values
         $validProducts = array_filter($validProducts, fn ($value) => !is_null($value));
 
         // sorting array by name, to mix both brazilian and europen products
         return array_values(Arr::sort($validProducts, function ($product) {
-            if (isset($product['nome'])) return $product['nome'];
-            if (isset($product['name'])) return $product['name'];
+            return $product['name'];
         }));
 
         return $validProducts;
