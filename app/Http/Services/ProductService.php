a5cb6a6b46cf564a2fea9681ab337daaddcd3143
Aureane Moraes <aureane.moraes@outlook.com>
2023-04-13 14:39:27 -0300

    feat: adicionada passagem de filtros nos parametros

--- app/Http/Services/ProductService.php
+++ app/Http/Services/ProductService.php
@@ -3,10 +3,17 @@
 namespace App\Http\Services;
 
 use App\Http\Rules\ListAllProductsRule;
+use App\Http\Rules\ListProductsBySupplier;
+use App\Models\Supplier;
 
 class ProductService {
-    public function listAllProducts()
+    public function listAllProducts($filters)
     {
-        return (new ListAllProductsRule())->listAllProducts();
+        return (new ListAllProductsRule())->listAllProducts($filters);
+    }
+
+    public function listProductsBySupplier($suplierId, $filters)
+    {
+        return (new ListProductsBySupplier())->listProductsBySupplier($suplierId, $filters);
     }
 }
